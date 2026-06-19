23a217907fff8f6683a13520446df221c4983e5b
UTFPR - FB <contato@utfpr.edu.br>
2026-06-19 14:47:37 -0300

    OK

--- site/js/app.js
+++ site/js/app.js
@@ -312,7 +312,7 @@ document.addEventListener('DOMContentLoaded', () => {
       <p class="capa-subtitle">${page.subtitle}</p>
       
       <div class="video-container">
-        <iframe src="https://www.youtube.com/embed/5bcIIwg_gGQ?si=wfwsdIex0IA_S_Xz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/5bcIIwg_gGQ?si=wfwsdIex0IA_S_Xz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
       
       <div class="capa-bullets">
