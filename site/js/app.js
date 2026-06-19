c8133c31cd9586549a518a71a8bd6e001e448508
UTFPR - FB <contato@utfpr.edu.br>
2026-06-19 14:37:02 -0300

    Adiciona iframe do Youtube na capa de forma responsiva

--- site/js/app.js
+++ site/js/app.js
@@ -311,6 +311,10 @@ document.addEventListener('DOMContentLoaded', () => {
       <h1 class="capa-title">${page.title}</h1>
       <p class="capa-subtitle">${page.subtitle}</p>
       
+      <div class="video-container">
+        <iframe src="https://www.youtube.com/embed/5bcIIwg_gGQ?si=wfwsdIex0IA_S_Xz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      
       <div class="capa-bullets">
         ${bulletsHtml}
       </div>
